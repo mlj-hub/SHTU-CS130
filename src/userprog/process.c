7fa6e93a7cdb4f5d38619fae3b2474fff8287de9
malj <malj@shanghaitech.edu.cn>
2022-11-02 11:11:57 +0800

    print infomation when process exits

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -143,6 +143,7 @@ process_exit (void)
 {
   struct thread *cur = thread_current ();
   uint32_t *pd;
+  printf ("%s: exit(%d)\n",cur->name,cur->child_info->exit_status);
 
   /* Destroy the current process's page directory and switch back
      to the kernel-only page directory. */
