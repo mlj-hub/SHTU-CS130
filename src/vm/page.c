0013649926a0b42c2d0df0bbd7f79e32289dc629
malj <malj@shanghaitech.edu.cn>
2022-12-03 01:06:42 +0800

    fix stack grow and finish load from file

--- src/vm/page.c
+++ src/vm/page.c
@@ -3,62 +3,70 @@
 #include "userprog/process.h"
 #include "userprog/syscall.h"
 #include "vm/frame.h"
+#include "threads/vaddr.h"
+#include "threads/palloc.h"
+#include "lib/string.h"
+#include "userprog/process.h"
 
+/* Load a page into memory according to the supplemental page entry */
 bool 
 load_page(struct supl_page_entry * t)
 {
-    /*load_page_in_supl_page_entry*/
-    if(t->type == 1)
-    {
-
-    }
-
-    /*load_page_in_swap*/
-    if(t->type == 2)
-    {
-
-    }
-
-    /*load_page_in_mmap*/
-    if(t->type == 3)
-    {
-
-    }
-
-    return true;
+  /* get a page from the memory */
+  uint32_t kaddr = frame_allocate(PAL_USER);
+  if(!kaddr)
+    return false;
+  bool success = true;
+  switch(t->type)
+  {
+    case Type_MMP:
+      thread_acquire_file_lock();
+      file_read_at(t->file,(void *)kaddr, t->file_size, t->file_ofs);
+      thread_release_file_lock();
+      t->kaddr = kaddr;
+      t->resident = true;
+      if(t->file_size != PGSIZE)
+        memset(t->kaddr+t->file_size,0,PGSIZE-t->file_size);     
+      if(!install_page(t->uaddr,kaddr,true))
+        success = false;
+      break;
+    case Type_Swap:
+      break;
+    default:
+      PANIC("Unkonwn page type");
+      break;
+  }
+  return success;
 }
 
 bool
-grow_stack(void * t,void * fault_addr)
+grow_stack(void * fault_addr)
 {
-    if(t == NULL){return false;}
+  struct supl_page_entry * new_page_entry = malloc(sizeof(struct supl_page_entry));
 
-    struct supl_page_entry * new_page_entry = malloc(sizeof(struct supl_page_entry));
+  if (new_page_entry == NULL)
+    return false;
 
-    if (new_page_entry == NULL) {
-		return false;
-	}
-
-    uint32_t * new_frame = frame_allocate(PAL_USER);
+  uint32_t * new_frame = frame_allocate(PAL_USER);
 
 	if (new_frame == NULL) {
 		free(new_page_entry);
 		return false;
 	}
 
-    /*set up new entry!!!*/
-    new_page_entry->owner = thread_current();
-    new_page_entry->type = 4;
-    new_page_entry->kaddr = new_frame;
-    new_page_entry->uaddr = fault_addr;
-    new_page_entry->writable = true;
+  /*set up new entry!!!*/
+  new_page_entry->type = 4;
+  new_page_entry->kaddr = new_frame;
+  new_page_entry->uaddr = fault_addr;
+  new_page_entry->writable = true;
+  new_page_entry->resident = true;
 
     /*install_page!!!*/
 	if (!install_page(new_page_entry->uaddr, new_frame,
         new_page_entry->writable)) 
 	{
 		free(new_page_entry);
-		free(new_frame);
+		frame_free(new_frame);
 		return false;
 	}
 
@@ -68,7 +76,7 @@ grow_stack(void * t,void * fault_addr)
 	struct lock * supl_page_table_lock = &thread_current()->supl_page_table_lock;
 
 	if (!lock_held_by_current_thread(&thread_current()->supl_page_table_lock))
-    {
+  {
 		lock_acquire(&thread_current()->supl_page_table_lock);
 	}
     
--- src/vm/page.h
+++ src/vm/page.h
@@ -4,23 +4,37 @@
 #include "threads/thread.h"
 #include <stdint.h>
 
+#define Type_Swap  1
+#define Type_MMP   2
+
+#define STACK_LIMIT 0x800000
+
 struct supl_page_entry
  {
     struct list_elem elem;
+    /* User virtual address to this page */
     uint32_t uaddr;
+    /* The corresponding kernel virtual address */
     uint32_t kaddr;
 
-    /* use type to determine the type of page fault
-       CODE_type=1; SWAP_type=2; MMAP_type=3;
-       STACK_type=4; FILE_type=5. */
-    uint8_t type;
-    struct thread * owner;
+    /* Type of this page */
+    int type;
+    /* If the page is writable */
     bool writable;
+    /* If the page is in the memory or not */
+    bool resident;
+
+    /* File which map to this page */
+    struct file * file;
+    /* File offset for the file */
+    int file_ofs;
+    /* Size of file in this page */
+    int file_size;
  };
 
 
 bool load_page(struct supl_page_entry * t);
 
-bool grow_stack(void * t,void * fault_addr);
+bool grow_stack(void * fault_addr);
 
 #endif
